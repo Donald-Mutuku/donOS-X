41307aa28b1c2001dbe047915e87009e7033ebf1
Donald <theredhotcoffee@gmail.com>
2026-08-23 22:03:36 +0300

    made functional notepad app

--- script.js
+++ script.js
@@ -56,6 +56,13 @@ function initWindow(element) {
         });
     }
 
+    var maxButton = document.getElementById(element.id + "max")
+    if (maxButton) {
+        maxButton.addEventListener("click", function() {
+            maximizeWindow(element);
+        });
+    }
+
     var closeButtons = element.querySelectorAll("[data-action='close']");
     closeButtons.forEach(function(btn) {
     btn.addEventListener("click", function() {
@@ -90,11 +97,6 @@ function dragElement(element) {
         element.style.top = `${clampedTop}px`;
         element.style.left = `${clampedLeft}px`;
 
-        // if the window is being dragged, we glue the cursor to its grab position
-        if (isDragging) {
-            virtualCursor.style.left = `${clampedLeft + grabOffsetX}px`;
-            virtualCursor.style.top = `${clampedTop + grabOffsetY}px`;
-        }
     }
 
     if (document.getElementById(element.id + "header")) {
@@ -132,14 +134,20 @@ function dragElement(element) {
         // this calculates the change in position from the original click point, instead of from cumulative click points, therefore snapping is more accurate
         const deltaX = e.clientX - startMouseX;
         const deltaY = e.clientY - startMouseY;
-
         clampPosition(startWinTop + deltaY, startWinLeft + deltaX);
+
+        const rect = screen.getBoundingClientRect();
+        let x = e.clientX - rect.left;
+        let y = e.clientY - rect.top;
+
+        x = Math.max(0, Math.min(x, rect.width));
+        y = Math.max(0, Math.min(y, rect.height));
+
+        virtualCursor.style.left = `${x}px`;
+        virtualCursor.style.top = `${y}px`;
     }
 
     function stopDragging () {
-        // this makes sure the cursor snaps back to where it first grabbed the window
-        virtualCursor.style.left = `${element.offsetLeft + grabOffsetX}px`;
-        virtualCursor.style.top = `${element.offsetTop + grabOffsetY}px`;
         // turns off this flag so that the cursor is free to move again
         isDragging = false;
         document.onmouseup = null;
@@ -174,3 +182,128 @@ function openWindow(element) {
         element.style.top = `${Math.max(0, centerY)}px`
     }
 }
+
+function maximizeWindow(element) {
+    if (element.isMax === true) {
+        element.style.width = element.dataset.initWidth;
+        element.style.height = element.dataset.initHeight;
+        element.style.top = element.dataset.topCoords;
+        element.style.left = element.dataset.leftCoords;
+        element.isMax = false;
+        return;
+    }
+    
+    // save the window coordinates and window size before the window was maximized
+    element.dataset.topCoords = element.style.top;
+    element.dataset.leftCoords = element.style.left;
+    element.dataset.initWidth = element.style.width;
+    element.dataset.initHeight = element.style.height;
+
+    element.style.width = '100%';
+    element.style.height = '100%';
+    element.style.placeSelf = 'center';
+    element.style.inset = 0;
+    element.isMax = true;
+}
+
+// Text formatting functions
+
+const boldBtn = document.getElementById('boldBtn');
+const italicsBtn = document.getElementById('italicsBtn');
+const notesField = document.getElementById('notesField');
+
+boldBtn.addEventListener('mousedown', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    document.execCommand('bold', false, null);
+    updateState();
+});
+
+italicsBtn.addEventListener('mousedown', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    document.execCommand('italic', false, null);
+    updateState();
+});
+
+// To update the text formatting buttons when they are clicked, or when content is bold/italic etc.
+notesField.addEventListener('keyup', updateState);
+notesField.addEventListener('keydown', updateState);
+
+function updateState() {
+    const isBold = document.queryCommandState('bold');
+    const isItalics = document.queryCommandState('italic');
+
+    boldBtn.classList.toggle('active', isBold);
+    italicsBtn.classList.toggle('active', isItalics);
+}
+
+const icontainer = document.getElementById('icontainer');
+const notes = document.getElementById('notes')
+const fileNameDisplay = document.getElementById("fileNameDisplay");
+
+let currentFile = null;
+
+const saveBtn = document.getElementById('save-btn');
+saveBtn.addEventListener('click', function(){
+    if (currentFile) {
+        localStorage.setItem(currentFile, notesField.innerHTML);
+    }
+
+    else {
+        const empty = notesField.textContent.replace(/\u00a0/g, ' ').trim() === "";
+
+        if (empty) {
+            alert("You can't save an empty file >:(");
+            return;
+        }
+        let fileName = prompt('Please enter a file name:');
+        if (!fileName) {
+            return;
+        }
+        if (!fileName.endsWith('.txt')) {
+            fileName += '.txt';
+        }
+        localStorage.setItem(fileName, notesField.innerHTML);
+        currentFile = fileName;
+        fileNameDisplay.textContent = fileName;
+
+        const div = document.createElement('div')
+        div.classList.add('desktopIcon');
+        div.innerHTML = `<img src=icons/notesicon.svg> <p>${fileName}</p>`;
+        icontainer.appendChild(div);
+
+        div.addEventListener('dblclick', function() {
+            openWindow(notes);
+            var fetched = localStorage.getItem(fileName);
+            notesField.innerHTML = fetched;
+            var fileNameDisplay = document.getElementById("fileNameDisplay");
+            fileNameDisplay.textContent = fileName;
+        });
+    }
+});
+
+const newBtn = document.getElementById("new-btn");
+newBtn.addEventListener("click", function(e) {
+    e.stopPropagation();
+    currentFile = null;
+    notesField.innerHTML = "";
+
+    fileNameDisplay.textContent = "";
+});
+
+// for some reason, if u pressed backspace when the notesField was empty, it messed with the padding of the entire window a bit
+// to fix that:
+notesField.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace') {
+        // first clean up break tags inserted by the browser, or white space that isn't visible
+        const cleanUp = notesField.innerHTML.replace(/br\s*\?>/gi, '').trim();
+        // then, if cleanUp is empty
+        if (cleanUp === '') {
+            // first, we tell the browser "yo, chill with the random <br> tags"
+            e.preventDefault();
+            // then we keep the container emptyy
+            notesField.innerHTML = '';
+        }
+    }
+});
